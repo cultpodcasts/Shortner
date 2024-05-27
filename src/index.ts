8a819d4d0f6dc575db36d9de0b003d3704752886
Jon Breen <jonbreen@outlook.com>
2024-05-28 00:47:53 +0100

    add-logging

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const keyRegex = /\/([A-Za-z0-9+\-/]{22})$/;
+		let dataPoint: AnalyticsEngineDataPoint = { indexes: [], blobs: [] };
+		let resp = new Response('Not Found', { status: 404 });
 		const matches = keyRegex.exec(request.url);
 		if (matches) {
-			console.log("matches "+ matches[1])
 			const key = matches[1];
+			dataPoint.indexes!.push(key);
 			try {
 				const redirectPath = await env.kv.get(key);
 				if (redirectPath) {
-					console.log("has key: "+redirectPath)
 					const url = new URL(redirectPath, env.redirect);
-					return Response.redirect(url.toString());
+					dataPoint.blobs!.push(redirectPath);
+					resp = Response.redirect(url.toString());
+				} else {
+					dataPoint.blobs!.push("Key not found");
 				}
 			} catch (error) {
 				console.log(error);
 			}
+		} else {
+			let pathName = new URL(request.url).pathname;
+			if (pathName.length > 96) {
+				pathName = pathName.slice(0, 95);
+			}
+			dataPoint.indexes!.push(pathName);
+			dataPoint.blobs!.push("Path does not match");
 		}
-		return new Response('Not Found', { status: 404 });
-	},
+		try {
+			env.analytics.writeDataPoint(dataPoint);
+		} catch (error) {
+			console.log(error);
+		}
+		return resp;
+	}
 };
