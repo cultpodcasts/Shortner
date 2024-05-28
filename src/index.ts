c4da64a246d805534817c1bef35822aaea710f07
Jon Breen <jonbreen@outlook.com>
2024-05-28 08:43:33 +0100

    Enhanced logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,25 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const keyRegex = /\/([A-Za-z0-9+_\-/]{22})$/;
 		let dataPoint: AnalyticsEngineDataPoint = { indexes: [], blobs: [] };
+
+		let ipAddress: string = "";
+		let asn: string = "";
+		let city: string = "";
+		if (request.cf != undefined && request.cf) {
+			dataPoint.blobs!.push(request.cf.clientTrustScoretr as string);
+			asn = request.cf.asn as string;
+			dataPoint.blobs!.push(asn);
+			ipAddress = request.headers.get('cf-connecting-ip') as string
+			dataPoint.blobs!.push(ipAddress);
+			dataPoint.blobs!.push(request.headers.get('User-Agent') as string);
+			if (request.cf.city) {
+				city = request.cf.city as string;
+				dataPoint.blobs!.push(city);
+			}
+			if (request.cf.country) {
+				dataPoint.blobs!.push(request.cf.country as string);
+			}
+		}
 		let resp = new Response('Not Found', { status: 404 });
 		const matches = keyRegex.exec(request.url);
 		if (matches) {
